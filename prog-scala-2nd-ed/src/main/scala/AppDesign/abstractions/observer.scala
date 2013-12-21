fdc867bd499612cabecf552d12bb34de7b0f8310
Dean Wampler <dean@deanwampler.com>
2013-12-20 17:14:05 -0800

    Adding file that got "lost" in the import for the 1st edition code.

---
+++ prog-scala-2nd-ed/src/main/scala/AppDesign/abstractions/observer.scala
@@ -0,0 +1,19 @@
+// code-examples/AppDesign/abstractions/observer.scala
+
+package appdesign.abstractions.observer
+
+abstract class SubjectObserver {
+  type S <: Subject
+  type O <: Observer
+  
+  trait Subject {
+    self: S =>    // #1
+    private var observers = List[O]()
+    def addObserver(observer: O) = observers ::= observer
+    def notifyObservers = observers foreach (_.receiveUpdate(self))  // #2
+  }
+  
+  trait Observer {
+    def receiveUpdate(subject: S)
+  }
+}
