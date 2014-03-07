dd47a27e451705e60b83db3aaa9563d0238f702e
Dean Wampler <dean@deanwampler.com>
2014-03-07 06:42:08 -0800

    Saving work on OO basics chapter.

---
+++ prog-scala-2nd-ed/src/main/scala/BasicOOP/Person.java
@@ -0,0 +1,19 @@
+// src/main/scala/BasicOOP/Person.java
+
+package BasicOOP;
+
+public class Person {
+  private String name;
+  private int    age;
+
+  public Person(String name, int age) {
+    this.name = name;
+    this.age  = age;
+  }
+
+  public void   setName(String name) { this.name = name; }
+  public String getName() { return this.name; }
+
+  public void setAge(int age) { this.age = age; }
+  public int  getAge() { return this.age; }
+}
