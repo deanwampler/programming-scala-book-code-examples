5425b4d7a618af0fc6b2eff365fca389a179dcad
Dean Wampler <dean@deanwampler.com>
2014-04-27 23:01:49 -0500

    Bug fixes.

--- prog-scala-2nd-ed/src/main/scala/ObjectSystem/ui/widget.scala
+++ prog-scala-2nd-ed/src/main/scala/ObjectSystem/ui/widget.scala
@@ -6,15 +6,3 @@ abstract class Widget {
   def draw(): Unit
   override def toString() = "(widget)"
 }
-
-object Widget {
-  val ButtonExtractorRE = """\(button: label=([^,]+),\s+\(Widget\)\)""".r
-  val TextFieldExtractorRE = """\(textfield: text=([^,]+),\s+\(Widget\)\)""".r
-
-  def apply(specification: String): Option[Widget] = specification match {
-    case ButtonExtractorRE(label)   => new Some(new Button(label))
-    case TextFieldExtractorRE(text) => new Some(new TextField(text))
-    case _ => None
-  }
-}
-
