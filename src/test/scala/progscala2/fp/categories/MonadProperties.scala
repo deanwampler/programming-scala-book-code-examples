d5fe575e90313a6a675f6b166de2d0900d415b80
Dean Wampler <dean@deanwampler.com>
2014-08-16 20:04:53 -0700

    Trivial whitespace changes.

--- src/test/scala/progscala2/fp/categories/MonadProperties.scala
+++ src/test/scala/progscala2/fp/categories/MonadProperties.scala
@@ -10,10 +10,10 @@ class MonadProperties extends FunSpec with PropertyChecks {
   val f1: Int => Seq[Int] = i => 0 until 10 by ((math.abs(i) % 10) + 1)
 
   describe ("Monad law for unit") {
-    it ("works for Sequence Monads") {     
+    it ("works for Sequence Monads") {
       import SeqM._
       val unitInt: Int => Seq[Int] = (i:Int) => unit(i)
-      forAll { (i: Int) => 
+      forAll { (i: Int) =>
         val seq: Seq[Int] = Seq(i)
         assert( flatMap(unit(i))(f1)  === f1(i) )
         assert( flatMap(seq)(unitInt) === seq )
@@ -25,9 +25,9 @@ class MonadProperties extends FunSpec with PropertyChecks {
     it ("works for Sequence Monads") {
       val f2: Int => Seq[Int] = i => Seq(i+1)
       import SeqM._
-      forAll { (i: Int) => 
+      forAll { (i: Int) =>
         val seq = Seq(i)
-        assert( flatMap(flatMap(seq)(f1))(f2) === 
+        assert( flatMap(flatMap(seq)(f1))(f2) ===
                 flatMap(seq)(x => flatMap(f1(x))(f2)) )
       }
     }
