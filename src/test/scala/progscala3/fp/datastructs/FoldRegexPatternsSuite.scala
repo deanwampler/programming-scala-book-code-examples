0669e8db3065d1448fdd92f2ebf53f11ca2f0ff0
Dean Wampler <dean@concurrentthought.com>
2020-02-29 15:12:56 -0800

    Another example converted to a test.

---
+++ src/test/scala/progscala3/fp/datastructs/FoldRegexPatternsSuite.scala
@@ -0,0 +1,44 @@
+// src/test/scala/progscala3/fp/datastructs/FoldRegexPatternsSuite.scala
+package progscala3.fp.datastructs
+
+import munit._
+
+class FoldRegexPatternsSuite extends FunSuite {
+  test("Regex pattern matching used in a foldLeft") {
+    val ignoreRegex = """^\s*(#.*|\s*)$""".r                             // <1>
+    val kvRegex = """^\s*([^=]+)\s*=\s*([^#]+)\s*.*$""".r                // <2>
+
+    val properties = """
+      |# Book properties
+      |
+      |book.name = Programming Scala, Third Edition # A comment
+      |book.authors = Dean Wampler
+      | an unexpected line
+      |book.publisher = O'Reilly
+      |book.publication-year = 2020
+      |""".stripMargin                                                   // <3>
+
+    // Type aliases so the rest of the code is easier to understand
+    type Error = (Int,String)
+    type KV = (String,String)
+
+    // Parse each line, skipping expected
+    val kvPairs =
+      properties.split("\n").
+      zipWithIndex.
+      foldLeft(Vector.empty[Either[Error,KV]]) { case (vect, (line, n)) =>
+        if (ignoreRegex.matches(line)) vect
+        else line match {
+          case kvRegex(key, value) => vect :+ Right(key.trim -> value.trim)
+          case _ => vect :+ Left(n+1, line.trim)
+        }
+      }
+      println(kvPairs)
+    assert(kvPairs == Vector(
+      Right("book.name" -> "Programming Scala, Third Edition"),
+      Right("book.authors" -> "Dean Wampler"),
+      Left(6 -> "an unexpected line"),
+      Right("book.publisher" -> "O'Reilly"),
+      Right("book.publication-year" -> "2020")))
+  }
+}
