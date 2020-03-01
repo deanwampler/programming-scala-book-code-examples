cad1b4a4bbf9b19a0555e9c0b1d04214f7daa15b
Dean Wampler <dean@concurrentthought.com>
2020-03-01 14:18:24 -0800

    Final tweaks

--- src/script/scala/progscala3/introscala/Upper2.scala
+++ src/script/scala/progscala3/introscala/Upper2.scala
@@ -1,7 +1,7 @@
 // src/script/scala/progscala3/introscala/Upper2.scala
 
-object Upper {
+object Upper2 {
   def convert(strings: String*) = strings.map(_.toUpperCase())
 }
 
-assert(Upper.convert("Hello", "World!") == Seq("HELLO", "WORLD!"))
+assert(Upper2.convert("Hello", "World!") == Seq("HELLO", "WORLD!"))
