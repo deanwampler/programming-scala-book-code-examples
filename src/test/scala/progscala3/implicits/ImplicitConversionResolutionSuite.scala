7b19e8025db15232664abdcf964d0b6c76a28b7d
Dean Wampler <dean@concurrentthought.com>
2020-05-03 09:47:48 -0700

    Made the example easier to understand

--- src/test/scala/progscala3/implicits/ImplicitConversionResolutionSuite.scala
+++ src/test/scala/progscala3/implicits/ImplicitConversionResolutionSuite.scala
@@ -6,33 +6,34 @@ import scala.language.implicitConversions
 
 class ImplicitConversionResolutionSuite extends FunSuite {
 
-	case class Foo(s: String)
-	object Foo {
-	  implicit def fromString(s: String): Foo =
-	  	new Foo(s"implicit conversion: $s")
-	}
+  case class Foo(s: String)
+  object Foo {
+    implicit def fromString(s: String): Foo =
+      new Foo(s"Foo's implicit conversion: $s")
+  }
 
-	object implicits {
-	  import Foo._
-	  def mf(foo: Foo): String  = foo.s
-	  def ms(s: String): String = mf(s)
-	}
+  object scope1 {
+    import Foo._
+    def apply(foo: Foo): String  = foo.s
+  }
 
-	object implicits2 {
-	  implicit def overridingConversion(s: String): Foo =
-	    new Foo(s"object implicits2 conversion: $s")
+  object scope2 {
+    object implicits {
+      implicit def overridingConversion(s: String): Foo =
+        new Foo(s"scope2's implicit conversion: $s")
+    }
 
-	  def mf(foo: Foo): String  = foo.s
-	  def ms(s: String): String = mf(s)
-	}
+    def apply(foo: Foo): String  = foo.s
+  }
 
-	test("An implicit conversion method in scope is invoked to convert a type"){
-		assert(implicits.mf(Foo("mf")) == "mf")
-		assert(implicits.ms("ms") == "implicit conversion: ms")
-	}
+  test("Implicit conversion method in scope is invoked to convert a type"){
+    assert(scope1(Foo("no use of conversion")) == "no use of conversion")
+    assert(scope1("foo") == "Foo's implicit conversion: foo")
+  }
 
-	test("The closest implicit conversion in scope is invoked") {
-		assert(implicits2.mf(Foo("mf")) == "mf")
-		assert(implicits2.ms("ms") == "object implicits2 conversion: ms")
-	}
+  test("The closest implicit conversion in scope is invoked") {
+    import scope2.implicits._
+    assert(scope2(Foo("no use of conversion")) == "no use of conversion")
+    assert(scope2("foo") == "scope2's implicit conversion: foo")
+  }
 }
