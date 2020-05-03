60d12d2f3e701a394cea3ae33d7f9114bdf84e95
Dean Wampler <dean@concurrentthought.com>
2020-05-03 12:50:23 -0700

    Added a main routine.

--- src/main/scala/progscala3/forcomps/RemoveBlanks.scala
+++ src/main/scala/progscala3/forcomps/RemoveBlanks.scala
@@ -5,12 +5,25 @@ object RemoveBlanks {
 
   /**
    * Remove blank lines from the specified input file.
+   * Loading a whole file in memory isn't optimal...
    */
   def apply(path: String, compressWhiteSpace: Boolean = false): Seq[String] =
     for
-      line <- scala.io.Source.fromFile(path).getLines.toSeq             // <1>
-      if line.matches("""^\s*$""") == false                             // <2>
-      line2 = if compressWhiteSpace then line.trim.replaceAll("\\s+", " ") // <3>
-              else line
+      line <- scala.io.Source.fromFile(path).getLines.toSeq           // <1>
+      if line.matches("""^\s*$""") == false                           // <2>
+      line2 = if compressWhiteSpace then line.trim.replaceAll("\\s+", " ")
+              else line                                               // <3>
     yield line2                                                       // <4>
+
+  def main(args: Array[String]): Unit = {                             // <5>
+    args.foreach { file =>
+      val (f, nows) =                                                 // <6>
+        if file.startsWith("-") then (file.substring(1), true)
+        else (file, false)
+
+      println(s"File: $f")
+      val seq = RemoveBlanks(f, nows)
+      seq.foreach(println)                                            // <7>
+    }
+  }
 }
