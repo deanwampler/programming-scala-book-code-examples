ab935cee4c15c5ed0cb098ff5bd26b611bca9a3f
Dean Wampler <dean@concurrentthought.com>
2020-08-04 17:38:02 -0700

    removed

--- src/script/scala/progscala3/typelessdomore/MultilineStrings2.scala
+++
@@ -1,7 +0,0 @@
-// src/script/scala/progscala3/typelessdomore/MultilineStrings2.scala
-
-def goodbye(name: String) =
-  s"""xxxGoodbye, ${name}yyy
-  xxxCome again!yyy""".stripPrefix("xxx").stripSuffix("yyy")
-
-val bye = goodbye("Programming Scala")
