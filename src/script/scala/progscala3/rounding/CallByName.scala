be321d02ce1998b69f5578e1968e26d359704d08
Dean Wampler <dean@concurrentthought.com>
2020-08-06 07:12:23 -0700

    Reworked the comments

--- src/script/scala/progscala3/rounding/CallByName.scala
+++ src/script/scala/progscala3/rounding/CallByName.scala
@@ -4,18 +4,18 @@ import scala.annotation.tailrec
 @tailrec                                                             // <1>
 def continue(conditional: => Boolean)(body: => Unit): Unit =         // <2>
   if conditional then                                                // <3>
-    body                                                             // <4>
+    body
     continue(conditional)(body)
 
 var count = 0
-continue (count < 5) {                                               // <5>
+continue (count < 5) {                                               // <4>
   println(s"at $count")
   count += 1
 }
 assert(count == 5)
 
 count = 0
-continue (count < 5)                                                 // <6>
+continue (count < 5)                                                 // <5>
   println(s"at $count")
   count += 1
 assert(count == 5)
