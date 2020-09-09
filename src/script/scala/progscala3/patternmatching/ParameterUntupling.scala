730bdff78125a12d53490286c0ca03bd6fbfa3ec
Dean Wampler <dean@concurrentthought.com>
2020-09-08 19:54:25 -0500

    Indentation fix

--- src/script/scala/progscala3/patternmatching/ParameterUntupling.scala
+++ src/script/scala/progscala3/patternmatching/ParameterUntupling.scala
@@ -2,10 +2,10 @@
 
 val tuples = Seq((1,2,3), (4,5,6), (7,8,9))
 val counts1 = tuples.map {
-	case (x, y, z) => x + y + z
+  case (x, y, z) => x + y + z
 }
 val counts2 = tuples.map {
-	(x, y, z) => x + y + z
+  (x, y, z) => x + y + z
 }
 val counts3 = tuples.map(_+_+_)
 
@@ -13,8 +13,7 @@ assert(counts1 == List(6, 15, 24))
 assert(counts2 == List(6, 15, 24))
 assert(counts3 == List(6, 15, 24))
 
-
 val tuples2 = Seq((1,(2,3)), (4,(5,6)), (7,(8,9)))
 val counts2b = tuples2.map {
-	(x, (y, z)) => x + y + z
+  (x, (y, z)) => x + y + z
 }
