dc0550e9626b23c46f2ce2696a9aa565ef12c721
Dean Wampler <dean@concurrentthought.com>
2020-10-04 10:52:22 -0500

    bug fix

--- src/script/scala/progscala3/objectsystem/variance/MutableVariance.scala
+++ src/script/scala/progscala3/objectsystem/variance/MutableVariance.scala
@@ -1,28 +1,26 @@
 // tag::first[]
 // src/script/scala/progscala3/objectsystem/variance/MutableVariance.scala
 
-class CInvariant[A](var mut: A)
-class CCovariant[+A](var mut: A)            // ERROR
-class CContravariant[-A](var mut: A)        // ERROR
+class Invariant[A](var mut: A)
+class Covariant[+A](var mut: A)            // ERROR
+class Contravariant[-A](var mut: A)        // ERROR
 // end::first[]
 
-// tag::cinvariant[]
-class CInvariant[A](val mutInit: A):
-  private var _mut: A = mutInit
-  def mut_=(a: A): Unit = _mut = a
-  def mut: A = _mut
-// end::cinvariant[]
-
 // tag::second[]
-class CCovariant[+A](val mutInit: A):
-  private var _mut: A = mutInit
-  def mut_=(a: A): Unit = _mut = a
-  def mut: A = _mut
-
-class CContravariant[-A](val mutInit: A):
+class Invariant[A](val mutInit: A):
   private var _mut: A = mutInit
   def mut_=(a: A): Unit = _mut = a
   def mut: A = _mut
 // end::second[]
 
-val ci: CInvariant[C]
+// tag::third[]
+class Covariant[+A](val mutInit: A):
+  private var _mut: A = mutInit
+  def mut_=(a: A): Unit = _mut = a
+  def mut: A = _mut
+
+class Contravariant[-A](val mutInit: A):
+  private var _mut: A = mutInit
+  def mut_=(a: A): Unit = _mut = a
+  def mut: A = _mut
+// end::third[]
