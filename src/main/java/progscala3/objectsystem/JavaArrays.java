136225145e4bf22c01d5e75cce944365bdb05462
Dean Wampler <dean@concurrentthought.com>
2020-10-04 12:02:22 -0500

    Minor change

--- src/main/java/progscala3/objectsystem/JavaArrays.java
+++ src/main/java/progscala3/objectsystem/JavaArrays.java
@@ -5,7 +5,7 @@ public class JavaArrays {
   public static void main(String[] args) {
     Integer[] array1 = new Integer[] {
       Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3) };
-    Number[] array2 = array1;      // Compiles fine
+    Number[] array2 = array1;          // Compiles fine, but shouldn't!!
     array2[2] = Double.valueOf(3.14);  // Compiles, but throws a runtime error!
   }
 }
