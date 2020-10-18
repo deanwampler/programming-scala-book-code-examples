598a6994fc15d868dfb8604c8348e10a49be11fe
Dean Wampler <dean@concurrentthought.com>
2020-10-18 17:40:47 -0500

    MultiMap example

---
+++ src/script/scala/progscala3/collections/MultiMap.scala
@@ -0,0 +1,15 @@
+// src/script/scala/progscala3/collections/MultiMap.scala
+import collection.mutable.{ HashMap, MultiMap, Set }              // <1>
+
+val mm = new HashMap[Int, Set[String]] with MultiMap[Int, String] // <2>
+
+mm.addBinding(1, "a")                                             // <3>
+mm.addBinding(2, "b")
+mm.addBinding(1, "c")                                             // <4>
+
+mm.entryExists(1, _ == "a") == true                               // <5>
+mm.entryExists(1, _ == "b") == false
+mm.entryExists(2, _ == "b") == true
+
+mm.removeBinding(1, "a")                                          // <6>
+mm.entryExists(1, _ == "a") == false
