b6050221ce5405380a4b079d716e94dd4e815ae8
Dean Wampler <dean@concurrentthought.com>
2020-10-31 16:52:47 -0500

    save work

--- src/script/scala/progscala3/typesystem/bounds/LowerBounds2.scala
+++ src/script/scala/progscala3/typesystem/bounds/LowerBounds2.scala
@@ -1,21 +1,13 @@
 // src/script/scala/progscala3/typesystem/bounds/LowerBounds2.scala
 
-// Won't compile, because if +A is used, then "default" in +getOrElse+
-// must be a of type B, where B >: A.
-// case class Opt[+A](value: A = null) {
-//   def getOrElse(default: A): A = if (value != null) value else default
-// }
-
+// Consider an invariant option. This seems to work, but you run into problems
+// if you try, e.g., `val Opt[Parent] = Ope(new Child(1))`
 case class Opt[A](value: A = null):
   def getOrElse(default: A): A = if (value != null) value else default
 
-
-class Parent(val value: Int):                              // <1>
-  override def toString = s"${this.getClass.getName}($value)"
-
-class Child(value: Int) extends Parent(value)
-
-val p5: Parent = Opt[Parent](null).getOrElse(new Parent(10))
-// Neither of the following two expressions will compile:
-val p4: Parent = Opt(new Child(1)).getOrElse(new Parent(10))
-val p6: Parent = Opt[Child](null).getOrElse(new Parent(10))
+val a: Parent = Opt[Parent](null).getOrElse(new Parent(10))
+val b: Child  = Opt(new Child(1)).getOrElse(new Child(10))
+val c: Parent = Opt[Parent](null).getOrElse(new Parent(10))
+val d: Parent = Opt[Parent](null).getOrElse(new Child(10))
+val e: Parent  = Opt(new Parent(1)).getOrElse(new Child(10))
+val f: Parent  = Opt(new Child(1)).getOrElse(new Parent(10))
