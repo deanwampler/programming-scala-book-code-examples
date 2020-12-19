1b981f813a99bc63b04156334eeb7e73452db4a7
Dean Wampler <dean@concurrentthought.com>
2020-12-19 16:36:24 -0600

    Improved example, added Asciidoc tags

--- src/script/scala/progscala3/contexts/GivenImports.scala
+++ src/script/scala/progscala3/contexts/GivenImports.scala
@@ -1,3 +1,4 @@
+// tag::O1[]
 // src/script/scala/progscala3/contexts/GivenImports.scala
 
 object O1:
@@ -7,13 +8,17 @@ object O1:
   class C2
   given c1: C1 = new C1
   given c2: C2 = new C2
+// end::O1[]
 
+// tag::imports[]
 import O1._             // Import everything EXCEPT the givens, c1 and c2
 import O1.given         // Import ONLY the givens (of type C1 and C2)
 import O1.{given, _}    // Import everything, givens and non-givens in O1
 import O1.{given C1}    // Import just the given of type C1
 import O1.c1            // Import just the given c1 of type C1
+// end::imports[]
 
+// tag::O2[]
 object O2:
   class C1
   class C2
@@ -21,3 +26,4 @@ object O2:
   given C2 = new C2
   given intOrd: Ordering[Int] = summon[Ordering[Int]]
   given listOrd[T: Ordering]: Ordering[List[T]] = summon[Ordering[List[T]]]
+// end::O2[]
--- src/script/scala/progscala3/patternmatching/MatchTreeADTExhaustive.scala
+++ src/script/scala/progscala3/patternmatching/MatchTreeADTExhaustive.scala
@@ -3,18 +3,16 @@ import progscala3.patternmatching.Tree
 import Tree.{Branch, Leaf}
 
 val enumSeq: Seq[Tree[Int]] = Seq(Leaf(0), Branch(Leaf(6), Leaf(7)))
-for t <- enumSeq
-yield t match
+val tree1 = for t <- enumSeq yield t match
   case Branch(left, right) => (left, right)
   case Leaf(value) => value
-// val res0: Seq[Any] = List(0, (Leaf(6),Leaf(7)))
+// val tree1: Seq[Matchable] = List(0, (Leaf(6),Leaf(7)))
 
 import progscala3.patternmatching.{STree, SBranch, SLeaf}
 
 val sealedSeq: Seq[STree[Int]] = Seq(SLeaf(0), SBranch(SLeaf(6), SLeaf(7)))
-for t <- sealedSeq
-yield t match
+val tree2 = for t <- sealedSeq yield t match
   case SBranch(left, right) => (left, right)
   case SLeaf(value) => value
-// val res1: Seq[Any] = List(0, (SLeaf(6),SLeaf(7)))
+// val tree2: Seq[Matchable] = List(0, (SLeaf(6),SLeaf(7)))
 
