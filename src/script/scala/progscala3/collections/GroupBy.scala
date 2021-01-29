f04a5cb6f4d414a89c2ed757d99f2e1dbc2322b7
Dean Wampler <dean@concurrentthought.com>
2021-01-28 23:03:30 -0600

    Refined examples

--- src/script/scala/progscala3/collections/GroupBy.scala
+++ src/script/scala/progscala3/collections/GroupBy.scala
@@ -1,6 +1,6 @@
 // src/script/scala/progscala3/collections/GroupBy.scala
 
-val ints = (1 until 100).toList
+val ints = (1 until 100).toVector
 
 val thirds = ints.groupBy(_%3)                                   // <1>
 val thirds1a = thirds.view.mapValues(ns => ns.map(n => (n,2*n))) // <2>
