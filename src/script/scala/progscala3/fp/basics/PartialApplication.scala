062c1f0f7bb1a78f17c17546fb96ae4aed17f731
Dean Wampler <dean@concurrentthought.com>
2021-02-03 22:57:17 -0600

    Improved example

--- src/script/scala/progscala3/fp/basics/PartialApplication.scala
+++ src/script/scala/progscala3/fp/basics/PartialApplication.scala
@@ -3,13 +3,21 @@
 def cat1(s1: String)(s2: String) = s1 + s2
 def cat2(s1: String) = (s2: String) => s1 + s2
 
-cat1("hello")("world")
-val cat1b = cat1("hello")
-cat1b("world")
+cat1("hello")("world")      // Call with both parameter lists.
+cat2("hello")("world")      // Same usage!
 
-cat2("hello")("world")
-val cat2b = cat2("hello")
-cat2b("world")
+val fcat1a = cat1           // No arguments applied
+fcat1a("hello")             // One argument list applied
+fcat1a("hello")("world")    // Both argument lists applied
+
+val fcat1b = cat1("hello")  // One argument list applied
+fcat1b("world")             // Second argument list applied
+
+val fcat2a = cat2           // No arguments applied
+fcat2a("hello")             // One argument list applied
+fcat2a("hello")("world")    // Both argument lists applied
+val fcat2b = cat2("hello")  // One argument list applied
+fcat2b("world")             // Second argument list applied
 
 val cat2F = (s1: String) => (s2: String) => s1+s2
 val cat2Fb: String =>  String => String  = s1 => s2 => s1+s2
