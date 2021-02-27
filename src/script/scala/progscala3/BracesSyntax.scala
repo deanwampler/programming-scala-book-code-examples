b353caf002deaaaaef463128702b2ed0142d4b02
Dean Wampler <dean@concurrentthought.com>
2021-02-27 11:52:21 -0600

    Refined the braceless examples with end markers.

--- src/script/scala/progscala3/BracesSyntax.scala
+++ src/script/scala/progscala3/BracesSyntax.scala
@@ -1,28 +1,31 @@
 // src/script/scala/progscala3/BracesSyntax.scala
 
-// For completeness, Scala 2 imports, not allowed in Scala 3:
+// For completeness, Scala 2 imports. Still allowed in Scala 3.0:
 import scala.concurrent._
 import java.util.{Queue => JQueue}
-import java.util.{HashMap => JHashMap, HashSet => _}
+import java.util.{*, HashMap => _}
 
 // Braces syntax - valid in both Scala 2 and 3
+// Braces around single expressions can be omitted, but they
+// are shown to represent the general case.
 
-// Package definition
-// package mypkg {  // Can't declare a package in a script!
-//   // stuff under mypkg...
+// Package definition.
+// Commented out because we can't declare a package in a script!
+// package mypkg {
+//   stuff under mypkg...
 // }
 
 // For comprehension
 val evens = for {
   i <- 0 until 10
   if i%2 == 0
-} yield i
+} yield { i }
 
 // For loop
-for {
+for {   // Can replace with () if there is a single generator: i <- ...
   i <- 0 until 10
   if i%2 == 0
-} println(i)
+} { println(i) }
 
 if (8 < 10) {
   println(true)
@@ -41,7 +44,7 @@ while (i < 10) { i+=1 }
 }
 
 // Partially-defined function
-val o:Option[Int] => Int = {
+val opt: Option[Int] => Int = {
   case Some(i) => i
   case None => 0
 }
@@ -50,12 +53,13 @@ val o:Option[Int] => Int = {
 import scala.io.Source
 import scala.util.control.NonFatal
 var source: Option[Source] = None
-try {
-  source = Some(Source.fromFile("..."))
+try {        // Can omit {} if nothing else is done with source...
+  source = Some(Source.fromFile("README.md"))
   // do something with it
-} catch {
+} catch {    // However, the {} are required for catch clauses
   case NonFatal(ex) => println(ex)
-} finally {
+}
+} finally {  // Can omit {} for both finally and if here.
   if (source != None) {
     source.get.close
   }
@@ -81,10 +85,16 @@ val mon = new Monoid[Int] {
   def zero: Int = 0
 }
 
-// New type class definition
-given intMonoid: Monoid[Int] with {
-  def add(i1: Int, i2: Int): Int = i1+i2
-  def zero: Int = 0
+// New type class given instantiation
+given intMonoid: Monoid[Float] with {
+  def add(f1: Float, f2: Float): Float = f1+f2
+  def zero: Float = 0.0F
+}
+
+// Alias given
+given Monoid[String] = new Monoid[String] {
+  def add(s1: String, s2: String): String = s1+s2
+  def zero: String = ""
 }
 
 // Extension method definition
--- src/script/scala/progscala3/IndentationSyntax.scala
+++ src/script/scala/progscala3/IndentationSyntax.scala
@@ -3,13 +3,16 @@
 // For completeness, Scala 3 imports:
 import scala.concurrent.{given, *}
 import java.util.Queue as JQueue
-import java.util.{HashMap as JHashMap, HashSet as _}
+import java.util.{*, HashMap as _}
 
-// Significant indentation syntax
+// Significant indentation syntax.
+// All the +end identifier+ markers are optional!
 
 // Package definition
-// package mypkg:   // Can't declare a package in a script!
-  // stuff under mypkg...
+// Commented out because we can't declare a package in a script!
+// package mypkg:
+//   stuff under mypkg...
+// end mypkg
 
 // For comprehension
 val evens = for
@@ -23,62 +26,109 @@ for
   if i%2 == 0
 do println(i)
 
+for
+  i <- 0 until 10
+  if i%2 == 0
+do      // For longer do blocks.
+  println(i)
+end for
+
 // If expression using the new control syntax with "then"
 if 8 < 10 then
   println(true)
 else
   println(false)
+end if
 
 // While loop using the new control syntax with "do"
 var i = 0
-while i < 10 do { i+=1 }
+while i < 10 do i+=1
+
+while i < 20
+do     // For longer do blocks.
+  i+=1
+end while
 
 // Match expression
 0 match
   case 0 => println("zero")
   case _ => println("other value")
+end match
 
 // Partially-defined function
-val o:Option[Int] => Int =
+val opt: Option[Int] => Int =
   case Some(i) => i
   case None => 0
+end opt
 
 // Try, catch, finally expression
 import scala.io.Source
 import scala.util.control.NonFatal
 var source: Option[Source] = None
 try
-  source = Some(Source.fromFile("..."))
-  // do something with it
+  source = Some(Source.fromFile("README.md"))
+  // ...
 catch
   case NonFatal(ex) => println(ex)
 finally
   if source != None then
     source.get.close
+  end if
+end try
 
-// Multiline method definition
+// Multiline method definition.
 def multiline(s: String): String =
   println(s"input: $s")
   val result = s.toUpperCase
   println(s"output: $result")
   result
+end multiline  // for constructors, use their name: this
+
+// Multiline value definition
+val foo =
+  println("Hi World!")
+  42
+end foo  // val doesn't work in this case, but...
+
+val (foo, bar) =
+  "foo" -> "bar"
+end val  // here's when you use val.
 
 // Trait, class, and object definition
 trait Monoid[A]:
   def add(a1: A, a2: A): A
   def zero: A
+end Monoid
 
 // Instantiate an anonymous instance
-val mon = new Monoid[Int]:
-  def add(i1: Int, i2: Int): Int = i1+i2
-  def zero: Int = 0
+val intMon = new Monoid[Int]:
+    def add(i1: Int, i2: Int): Int = i1+i2
+    def zero: Int = 0
+  // Can't use end ... here because of the first line, but...
+val longMon =
+  new Monoid[Long]:
+    def add(l1: Long, l2: Long): Long = l1+l2
+    def zero: Long = 0L
+  end new // You can use "end new" here because "new Monoid..." starts at the same column!
 
-// New type class definition
-given intMonoid: Monoid[Int] with
-  def add(i1: Int, i2: Int): Int = i1+i2
-  def zero: Int = 0
+// New type class given instantiation
+given floatMonoid: Monoid[Float] with
+  def add(f1: Float, f2: Float): Float = f1+f2
+  def zero: Float = 0.0F
+end floatMonoid  // Use identifier.
+given Monoid[Double] with
+  def add(d1: Double, d2: Double): Double = d1+d2
+  def zero: Double = 0.0
+end given        // Anonymous, so no identifier. Hence, use "given".
+
+// Alias given
+given Monoid[String] = new Monoid[String]:
+  def add(s1: String, s2: String): String = s1+s2
+  def zero: String = ""
+end given
 
 // Extension method definition
 extension (s: String)
   def bold: String = s.toUpperCase + "!"
   def meek: String = "(" + s.toLowerCase + ", maybe?)"
+end extension
