4c560217ab0c1a9061362657edc743afe6f5c107
Dean Wampler <dean@concurrentthought.com>
2021-03-08 09:39:51 -0600

    Refined the definition of Functor

--- src/main/scala/progscala3/typesystem/typelambdas/Functor.scala
+++ src/main/scala/progscala3/typesystem/typelambdas/Functor.scala
@@ -1,16 +1,16 @@
 // src/main/scala/progscala3/typesystem/typelambdas/Functor.scala
 package progscala3.typesystem.typelambdas
 
-trait Functor[A, M[_]]:
-  extension (m: M[A]) def map2[B](f: A => B): M[B]
+trait Functor[M[_]]:
+  extension [A] (m: M[A]) def map2[B](f: A => B): M[B]
 
 object Functor:
-  given [A]: Functor[A, Seq] with
-    extension (seq: Seq[A]) def map2[B](f: A => B): Seq[B] = seq map f
+  given Functor[Seq] with
+    extension [A] (seq: Seq[A]) def map2[B](f: A => B): Seq[B] = seq map f
 
   type MapKV = [K] =>> [V] =>> Map[K,V]                         // <1>
 
-  given [K, V1]: Functor[V1, MapKV[K]] with                     // <2>
-    extension (map: MapKV[K][V1])
+  given [K]: Functor[MapKV[K]] with                             // <2>
+    extension [V1] (map: MapKV[K][V1])
       def map2[V2](f: V1 => V2): MapKV[K][V2] = map.view.mapValues(f).toMap
 
