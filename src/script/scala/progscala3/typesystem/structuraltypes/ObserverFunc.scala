f61e5bbc45b780fa7c754c8abfe53a3ae8429615
Dean Wampler <dean@concurrentthought.com>
2021-03-14 15:37:15 -0500

    Fixed ObserverFunc to be more realistic

--- src/script/scala/progscala3/typesystem/structuraltypes/ObserverFunc.scala
+++ src/script/scala/progscala3/typesystem/structuraltypes/ObserverFunc.scala
@@ -1,24 +1,24 @@
 // src/script/scala/progscala3/typesystem/structuraltypes/ObserverFunc.scala
-import progscala3.typesystem.structuraltypes.SubjectFunc             // <1>
+import progscala3.typesystem.structuraltypes.SubjectFunc        // <1>
 
-case class Counter(start: Int = 0) extends SubjectFunc:              // <2>
+case class Counter(start: Int = 0) extends SubjectFunc:         // <2>
   type State = Int
   var count = start
   def increment(): Unit =
     count += 1
     notifyObservers(count)
 
-var observed1: Int = 0                                               // <3>
-var observed2: Int = 0
-
-val observer1: Int => Unit = i => observed1 += 1  // ignore i
-val observer2: Int => Unit = i => observed2 += 1  // ignore i
+case class CounterObserver(var updateCalledCount: Int = 0) {    // <3>
+  def apply(count: Int): Unit = updateCalledCount += 1
+}
+val observer1 = CounterObserver()
+val observer2 = CounterObserver()
 
 val c = Counter()
 c.increment()
-c.addObserver(observer1)
+c.addObserver(observer1.apply)                                  // <4>
 c.increment()
-c.addObserver(observer2)
+c.addObserver(observer2.apply)
 c.increment()
 assert(c.count == 3)
 assert(observed1 == 2)
