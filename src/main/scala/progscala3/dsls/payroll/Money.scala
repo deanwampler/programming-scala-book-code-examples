f9e8d54b26848ae6b032266d3194079ec065187b
Dean Wampler <dean@concurrentthought.com>
2021-03-27 10:05:15 -0500

    Removed an unnecessary use of targetName

--- src/main/scala/progscala3/dsls/payroll/Money.scala
+++ src/main/scala/progscala3/dsls/payroll/Money.scala
@@ -1,8 +1,6 @@
 // src/main/scala/progscala3/dsls/payroll/Money.scala
 package progscala3.dsls.payroll
 import progscala3.contexts.accounting.*                         // <1>
-import scala.annotation.targetName
-
 import scala.util.FromDigits.Floating                           // <2>
 
 given Floating[Dollars] with                                    // <3>
@@ -12,7 +10,7 @@ given Floating[Percentage] with
   def fromDigits(digits: String): Percentage = Percentage(digits.toDouble)
 
 implicit class dsc(sc: StringContext):                          // <4>
-  @targetName("dollars") def $(tokens: Any*) =
+  def $(tokens: Any*) =
     val str = StringContextUtil.foldTokens(tokens.toSeq, sc.parts)
     Dollars(str.toDouble)
 
