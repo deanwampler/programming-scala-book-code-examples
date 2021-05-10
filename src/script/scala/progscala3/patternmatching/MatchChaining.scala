3b91644907b6632622af047be8400b1bb7ba3805
Dean Wampler <dean@concurrentthought.com>
2021-05-09 19:02:26 -0500

    Added an explanatory comment

--- src/script/scala/progscala3/patternmatching/MatchChaining.scala
+++ src/script/scala/progscala3/patternmatching/MatchChaining.scala
@@ -1,7 +1,7 @@
 // src/script/scala/progscala3/patternmatching/MatchChaining.scala
 
 for opt <- Seq(Some(1), None)
-yield opt match {
+yield opt match {  // Braces for clarity, but not required! Try removing them.
   case None => ""
   case Some(i) => i.toString
 } match {        // will match on the String from the previous match
