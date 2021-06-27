1e20ab02121ac5a96dfd226b89f7f4c04414116b
Dean Wampler <dean@concurrentthought.com>
2021-06-27 09:40:11 -0500

    Converted the InvariantSuite to a ScalaCheck test

--- src/test/scala/progscala3/meta/InvariantSuite.scala
+++ src/test/scala/progscala3/meta/InvariantSuite.scala
@@ -2,30 +2,40 @@
 package progscala3.meta
 
 import munit.*
+import org.scalacheck.*
 
-class InvariantSuite extends FunSuite:
+class InvariantSuite extends ScalaCheckSuite:
+  import Prop.{forAll, propBoolean}
+
   case class Variable(var i: Int, var s: String)
+  val genVariables =
+    for
+      i <- Gen.posNum[Int]
+      s <- Arbitrary.arbitrary[String]
+    yield Variable(i, s)
 
-  def succeed() =                                                  // <1>
-    val v = Variable(0, "Hello!")
-    val i1 = invariant(v.s == "Hello!") {                          // <2>
-      v.i += 1
-      v.i += 1
-      v.i
-    }
-    assert(i1 == 2)
-
-  test("invariant.apply should not fail if the invariant holds") { succeed() }
-
-  test("invariant.apply should return the value returned by the expressions") { succeed() }
-
-  test("invariant.apply should fail if the invariant is broken") {
-    intercept[invariant.InvariantFailure] {                        // <3>
-      val v = Variable(0, "Hello!")
-      invariant(v.s == "Hello!") {
-        v.i += 1
-        v.s = "Goodbye!"
-        v.i += 1
+  protected def passingInvariant =
+    forAll(genVariables) { variable =>
+      val s = variable.s
+      val i = variable.i
+      val i2 = invariant(variable.s == s) {
+        variable.i += 2
+        variable.i
       }
+      (i2 == i+2 && variable.s == s) :| s"i2=$i2, s=$s, variable = $variable"
+    }
+
+  property("invariant.apply should not fail if the invariant holds") { passingInvariant }
+  property("invariant.apply should return the value returned by the expressions") { passingInvariant }
+  property("invariant.apply should fail if the invariant is broken") {
+    forAll(genVariables) { variable =>
+      val s = variable.s
+      intercept[invariant.InvariantFailure] {
+        invariant(variable.s == s) {
+          variable.i += 2
+          variable.s = "bad"
+        }
+      }
+      true // hack to make the types check.
     }
   }
