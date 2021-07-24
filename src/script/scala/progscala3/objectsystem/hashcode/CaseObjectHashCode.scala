5177539c380b33f35d799648b906b73e259439fb
Dean Wampler <dean@concurrentthought.com>
2021-07-24 17:56:55 -0500

    Added equivalent examples of using ## as an alias for hashCode

--- src/script/scala/progscala3/objectsystem/hashcode/CaseObjectHashCode.scala
+++ src/script/scala/progscala3/objectsystem/hashcode/CaseObjectHashCode.scala
@@ -14,3 +14,12 @@ println(s"O3:           ${O3.hashCode} != ${"O3".hashCode}")
 println(s"O3.O4:        ${O3.O4.hashCode} != ${"O3.O4".hashCode}")
 println(s"O3.O4 vs. O4: ${O3.O4.hashCode} == ${"O4".hashCode}")
 println(s"O3.O4 vs. O3: ${O3.O4.hashCode} != ${"O3".hashCode}")
+
+// The following section was not shown in the Third Edition, First Printing.
+// There is an ## alias for hashCode, but I never use it:
+println(s"O1:           ${O1.##} == ${"O1".##}")
+println(s"O2:           ${O2.##} == ${"O2".##}")
+println(s"O3:           ${O3.##} != ${"O3".##}")
+println(s"O3.O4:        ${O3.O4.##} != ${"O3.O4".##}")
+println(s"O3.O4 vs. O4: ${O3.O4.##} == ${"O4".##}")
+println(s"O3.O4 vs. O3: ${O3.O4.##} != ${"O3".##}")
