b243b790c33551df1ba872733f5b7b1c73c9d8fc
Dean Wampler <dean.wampler@ibm.com>
2024-02-24 16:26:59 -0600

    Fixed synchronize bugs.

--- src/main/scala/progscala3/traits/ui/ButtonCountObserver.scala
+++ src/main/scala/progscala3/traits/ui/ButtonCountObserver.scala
@@ -2,7 +2,11 @@
 package progscala3.traits.ui
 import progscala3.traits.observer.*
 
+/*
+ * 2024-02-24: Changed the original count.synchronized {...} to this.synchronized {...},
+ * because the former was wrong for a primitive. So now we synchronize on the instance.
+ */
 class ButtonCountObserver extends Observer[Button]:
   var count = 0
   def receiveUpdate(state: Button): Unit =
-    count.synchronized { count += 1 }
+    this.synchronized { count += 1 } 
--- src/main/scala/progscala3/traits/ui2/CountObserver.scala
+++ src/main/scala/progscala3/traits/ui2/CountObserver.scala
@@ -2,6 +2,10 @@
 package progscala3.traits.ui2
 import progscala3.traits.observer.*
 
+/*
+ * 2024-02-24: Changed the original count.synchronized {...} to this.synchronized {...},
+ * because the former was wrong for a primitive. So now we synchronize on the instance.
+ */
 trait CountObserver[State] extends Observer[State]:
   var count = 0
-  def receiveUpdate(state: State): Unit = count.synchronized { count += 1 }
+  def receiveUpdate(state: State): Unit = this.synchronized { count += 1 }
